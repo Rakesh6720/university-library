91f54bc8210e7b35d9d5449ab2e8ba155708f9b3
Rakesh Baruah <rbaruah@gmail.com>
2025-01-24 08:03:32 -0600

    update home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,12 @@
+import BookList from "@/components/BookList";
+import BookOverview from "@/components/BookOverview";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <>
-      <Button>Click Me</Button>
+      <BookOverview />
+      <BookList />
     </>
   );
 }
